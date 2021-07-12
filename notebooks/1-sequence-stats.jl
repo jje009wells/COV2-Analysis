7011681806df8020e9d7c43de4af8527368cb31d
jje009wells <je100@wellesley.edu>
2021-07-11 22:36:50 -0400

    histogram added

--- notebooks/1-sequence-stats.jl
+++ notebooks/1-sequence-stats.jl
@@ -1,10 +1,14 @@
 ### A Pluto.jl notebook ###
-# v0.14.8 Analysis for the mean and standard deviation of COV2 genomes in cov2genseq.fasta
-# Coronavirus geneomes downloaded from NCBI (see `data/data.md` for more info)
-
+# v0.14.8
 
 using Markdown
 using InteractiveUtils
+
+# ╔═╡ 781f89b8-d61d-4e2c-9999-e0c86fe5732e
+using BioinformaticsBISC195
+
+# ╔═╡ 13535e83-f647-47b8-a0b9-0539103b6771
+using Plots
 
 # ╔═╡ 3324a4c1-0291-40d7-9869-7db1024f707a
 function fasta_header(header)
@@ -66,9 +70,29 @@ begin
 	# makes use of Statistics package functions mean() and std() to compute the analysis based on the data in the Vector created above
 	mean_cov2_length = mean(seq_lengths)
 	std_cov2_length = std(seq_lengths)
+	
+	println("Mean is $mean_cov2_length")
+	println("Std Dev is $std_cov2_length")
+		
+end
+
+# ╔═╡ 55de3beb-5c2a-4ffe-a0c9-156384595d3c
+histogram(seq_lengths, label = "", xaxis = "Sequence Lengths", yaxis = "Count")
+
+# ╔═╡ bb6c6a47-ecf7-42a6-aafb-c47e3a946ecf
+begin
+	#find the maximum and minimum lengths of the sequences in the file in order to help figure out if the sequences follow a normal distribution
+	max = maximum(seq_lengths)
+	println("Max is $max")
+	min = minimum(seq_lengths)
+	println("Min is $min")
 end
 
 # ╔═╡ Cell order:
+# ╠═781f89b8-d61d-4e2c-9999-e0c86fe5732e
+# ╠═13535e83-f647-47b8-a0b9-0539103b6771
+# ╠═55de3beb-5c2a-4ffe-a0c9-156384595d3c
 # ╠═3324a4c1-0291-40d7-9869-7db1024f707a
 # ╠═09c83c28-a88e-4d91-873d-8ade50ae691b
 # ╠═9cb89580-e0e4-11eb-08c7-ad14760a13d2
+# ╠═bb6c6a47-ecf7-42a6-aafb-c47e3a946ecf
