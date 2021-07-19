228bc7fc46d1f50247f7cbbd890cb37bbd3682f0
jje009wells <je100@wellesley.edu>
2021-07-19 04:14:19 -0400

    updated analysis proposal with Plan 2v2

--- notebooks/1-sequence-stats.jl
+++ notebooks/1-sequence-stats.jl
@@ -10,48 +10,6 @@ using BioinformaticsBISC195
 # ╔═╡ 13535e83-f647-47b8-a0b9-0539103b6771
 using Plots
 
-# ╔═╡ 3324a4c1-0291-40d7-9869-7db1024f707a
-function fasta_header(header)
-    #this is code to read the headers of the different sequences and break them up appropriately
-    startswith(header, '>') || error("Invalid header (headers must start with '>')")
-
-    splitVect = split(header[2:end], "|")
-    returnTuple = ()
-    for component in splitVect
-        returnTuple = (returnTuple..., strip(component))
-    end
-
-    return returnTuple
-end
-
-# ╔═╡ 09c83c28-a88e-4d91-873d-8ade50ae691b
-function parse_fasta(path)
-# this code parses the fasta file at the given path by storing each header and each individual sequence
-    headersVect = Vector()
-    seqVect = Vector()
-    toBeCombined = Vector()
-
-
-    for line in eachline(path)
-        #@info "line is " line
-        #@info "to be combined is" toBeCombined
-        if startswith(line, '>')
-            push!(headersVect, fasta_header(line))
-            if size(toBeCombined,1) > 0
-                push!(seqVect, join(toBeCombined))
-                toBeCombined = Vector()
-            end
-        elseif line != ""
-            push!(toBeCombined, line)
-
-        end
-        #@info "to be combined is" toBeCombined
-    end
-    push!(seqVect, join(toBeCombined))
-    
-    return (headersVect, seqVect)
-end
-
 # ╔═╡ 9cb89580-e0e4-11eb-08c7-ad14760a13d2
 begin
 	# Coronavirus mean and SD analysis
@@ -79,6 +37,48 @@ end
 # ╔═╡ 55de3beb-5c2a-4ffe-a0c9-156384595d3c
 histogram(seq_lengths, label = "", xaxis = "Sequence Lengths", yaxis = "Count")
 
+# ╔═╡ 3324a4c1-0291-40d7-9869-7db1024f707a
+#=function fasta_header(header)
+    #this is code to read the headers of the different sequences and break them up appropriately
+    startswith(header, '>') || error("Invalid header (headers must start with '>')")
+
+    splitVect = split(header[2:end], "|")
+    returnTuple = ()
+    for component in splitVect
+        returnTuple = (returnTuple..., strip(component))
+    end
+
+    return returnTuple
+end=#
+
+# ╔═╡ 09c83c28-a88e-4d91-873d-8ade50ae691b
+#=function parse_fasta(path)
+# this code parses the fasta file at the given path by storing each header and each individual sequence
+    headersVect = Vector()
+    seqVect = Vector()
+    toBeCombined = Vector()
+
+
+    for line in eachline(path)
+        #@info "line is " line
+        #@info "to be combined is" toBeCombined
+        if startswith(line, '>')
+            push!(headersVect, fasta_header(line))
+            if size(toBeCombined,1) > 0
+                push!(seqVect, join(toBeCombined))
+                toBeCombined = Vector()
+            end
+        elseif line != ""
+            push!(toBeCombined, line)
+
+        end
+        #@info "to be combined is" toBeCombined
+    end
+    push!(seqVect, join(toBeCombined))
+    
+    return (headersVect, seqVect)
+end=#
+
 # ╔═╡ bb6c6a47-ecf7-42a6-aafb-c47e3a946ecf
 begin
 	#find the maximum and minimum lengths of the sequences in the file in order to help figure out if the sequences follow a normal distribution
@@ -95,8 +95,8 @@ function filter25k(data)
 	newSeq = Vector()
 	i = 0
 	for i in 1:length(data[1])
-		seqlength = length(data[2][i])
-		@info "length is $seqlength"
+		#seqlength = length(data[2][i])
+		#@info "length is $seqlength"
 		if length(data[2][i]) > 25000
 			push!(newSeq, data[2][i])
 			push!(newHeaders, data[1][i])
