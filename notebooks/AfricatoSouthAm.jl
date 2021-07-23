57a43bf45aeb63b278479db001e46403a267acbd
jje009wells <je100@wellesley.edu>
2021-07-23 17:14:11 -0400

    comments cleanup

--- notebooks/AfricatoSouthAm.jl
+++ notebooks/AfricatoSouthAm.jl
@@ -12,7 +12,9 @@ using Plots
 
 # ╔═╡ f52e5561-ee04-4d95-bfce-a8574e7c31d9
 md"""
-First, I am going to parse each file that contains code specific to each country (Ghana, Egypt, Peru) so that I have each set of genomes available to use
+First, I am going to parse each file that contains code specific to each country (Ghana, Egypt, Peru) so that I have each set of genomes available to use. The files were obtained from: [NCBI](https://www.ncbi.nlm.nih.gov/labs/virus/vssi/#/virus?SeqType_s=Nucleotide&VirusLineage_ss=Severe%20acute%20respiratory%20syndrome%20coronavirus%202%20(SARS-CoV-2),%20taxid:2697049&VirusLineage_ss=Middle%20East%20respiratory%20syndrome-related%20coronavirus%20(MERS-CoV),%20taxid:1335626&VirusLineage_ss=Severe%20acute%20respiratory%20syndrome-related%20coronavirus,%20taxid:694009&QualNum_i=50&Completeness_s=complete&CreateDate_dt=2019-12-01T00:00:00.00Z%20TO%202021-07-20T23:59:59.00Z&CollectionDate_dr=2019-12-19T00:00:00.00Z%20TO%202021-07-20T23:59:59.00Z&Country_s=Egypt&Country_s=Ghana&Country_s=Peru)
+
+Each file for each country contains all SARS, COV2, and MERS genomes from Dec 2019 to July 2021, and is parsed using the parse_fasta function from the BioinformaticsBISC195 package.
 """
 
 # ╔═╡ c76fd317-869c-43d1-ad8f-735f13f2da46
@@ -26,16 +28,20 @@ genomes_peru = parse_fasta(joinpath(@__DIR__, "..", "data", "covgen_peru.fasta")
 
 # ╔═╡ dfb58341-dc84-4c37-a729-9e7afef2e5ee
 md"""
-Now, I want to get collections of all the kmers of a certain length for each country. I initially started with k = 3 as 3mers could represent specific amino acids that certain sequences have in common, but due to the sheer size of the data all 64 possible 3mers are represented in each graph and it is difficult to make comparisons across countries that way or pick out any specifc animo acids. 
-I ended up going with 12mers because the larger size has more potential possibilities and specific kemrs taken from the sequence get more specific, though this not a perfect solution either. 
+Now, I want to get collections of all the kmers of a certain length for each country. This combines data all sequences data from each country to attempt to make the data more generalized to the entire country, meaning each kmer in these sets exists every sequence in the country's file.
+I initially started with k = 3 as 3mers could represent specific amino acids that certain sequences have in common, but due to the sheer size of the data all 64 possible 3mers are represented in each graph and it is difficult to make comparisons across countries that way or pick out any specifc animo acids. 
+I ended up going with 12mers because the larger size has more potential possibilities and therefore the different sets of kmers per country can get more specific, though this not a perfect solution either.
+It would be interesting to change the different k lengths and run several tests to see how long and specific the kmers can get, but for these purposes 12 is about the longest I can go before the number of kmer possibilites becomes excessive.
 """
 
 # ╔═╡ 03ec5984-a462-410b-9403-24b4c0e94a21
-begin
-	kmers_allofghana = kmercollecting(genomes_ghana[2], 12)
-	kmers_allofegypt = kmercollecting(genomes_egypt[2], 12)
-	kmers_allofperu = kmercollecting(genomes_peru[2], 12)
-end
+kmers_allofghana = kmercollecting(genomes_ghana[2], 12)
+
+# ╔═╡ da281e5e-afe7-4fec-a709-424493561783
+kmers_allofegypt = kmercollecting(genomes_egypt[2], 12)
+
+# ╔═╡ 24c15bf4-9079-4aa4-97dd-e383a34fc718
+kmers_allofperu = kmercollecting(genomes_peru[2], 12)
 
 # ╔═╡ aacf8425-eeae-4f0d-b4b2-5dbd0daf5603
 md"""
@@ -53,7 +59,7 @@ kmers_peru = intersect(kmers_allofperu...)
 
 # ╔═╡ 8de6f315-e8b9-4677-be8f-63f632347677
 md"""
-I was initially going to use a function called kmercombining to get a collection that contains only 3mers that exist in Egypt and Ghana, and Egypt and Peru, but then found out a way to just use the preexisting intersect function for the same thing. I can later get the lengths of these to compare how many kmers are in common for each
+I was initially going to use a function called kmercombining to get a collection that contains only kmers that exist in Egypt and Ghana, and Egypt and Peru, but then found out a way to just use the preexisting intersect function for the same thing. I can later get the lengths of these to compare how many kmers are in common for each
 """
 
 # ╔═╡ 3652390f-28cb-4497-af79-7b245f78a893
@@ -65,50 +71,40 @@ peru_to_egypt = intersect([kmers_peru, kmers_egypt]...)
 # ╔═╡ b1a52631-6d3d-4f99-8b2c-7fc4ae15012e
 all_countries = intersect([kmers_ghana, kmers_egypt, kmers_peru]...)
 
-# ╔═╡ ad1f1d24-5f22-4d69-9338-cf8c1e1ec188
-#=function kmercollecting(set, k)
-    kmerdicts = Vector()
-	for seq in set
-		push!(kmerdicts, collect(keys(kmercount(seq, k))))
-	end
-	return kmerdicts	
-end=#
-
-# ╔═╡ e74a273a-9451-4d09-b68a-796b57bc9245
-#=function kmercombining(sets)
-	newint = sets[1]
-	for kmers in sets
-		#@info newint
-		newint = intersect(newint, kmers)
-	end
-	return newint
-end=#
-
-# ╔═╡ e32867e2-81fa-4e70-ae11-bdd41c0358ad
-# rather than re-write functions in your notebook, you can display the docs
-# that you've already written
-@doc parse_fasta
-
 # ╔═╡ be6670fa-a026-4838-b2f3-a94eee5c98b1
 md"""
 Now I want to get the data ready to plot. It will be a bar chart with the count of kmers on the y axis and the x axis that has bars for the number of kmers in common across the entire country for each country, the kmers in common for all countries combined, how many exist only in Egypt and Ghana, and how many exist only in Egypt and Peru.
 """
 
 # ╔═╡ f93f92fa-0e75-47c3-8fce-93c720a38f2e
-begin
-	only_in_ghana = setdiff([kmers_ghana, kmers_egypt, kmers_peru]...)
-	only_in_peru = setdiff([kmers_peru, kmers_egypt, kmers_ghana]...)
-	only_in_egypt = setdiff([kmers_egypt, kmers_ghana, kmers_peru]...)
-end
+only_in_ghana = setdiff([kmers_ghana, kmers_egypt, kmers_peru]...)
+
+# ╔═╡ f5beccb9-b9fa-4b42-afd4-c6ffe0857409
+only_in_peru = setdiff([kmers_peru, kmers_egypt, kmers_ghana]...)
+
+# ╔═╡ 3e10a923-60da-4db0-b856-de045d1faa5e
+only_in_egypt = setdiff([kmers_egypt, kmers_ghana, kmers_peru]...)
 
 # ╔═╡ cf88bfcf-2350-43c2-9136-16fea916ff9a
-begin
-	xaxis = ["Only Ghana", "Only Peru", "Only Egypt", "In All Countries", "Only Ghana and Egypt", "Only Peru and Egypt"]
-	yaxis = [length(only_in_ghana), length(only_in_peru), length(only_in_egypt), length(all_countries), length(ghana_to_egypt), length(peru_to_egypt)]
-end
+xaxis = ["Only Ghana", "Only Peru", "Only Egypt", "In All Countries", "Only Ghana and Egypt", "Only Peru and Egypt"]
+
+# ╔═╡ 626de1de-e12a-4e67-9d0c-eed4b9c74d1b
+yaxis = [length(only_in_ghana), length(only_in_peru), length(only_in_egypt), length(all_countries), length(ghana_to_egypt), length(peru_to_egypt)]
 
 # ╔═╡ 1eae0dbb-a20d-4daf-ab1b-c1ecc6b4858d
-bar(xaxis,yaxis, label = "", xaxis = "Locations", yaxis = "Kmer Count", bar_width = 0.5,size = (920,600))
+bar(xaxis,yaxis, label = "", xaxis = "Locations", yaxis = "Kmer Count", bar_width = 0.5,size = (900,600))
+
+# ╔═╡ 46c7eda6-98b7-4706-85c8-6922985e8f8a
+md"""
+Unlike what I predicted, there are actually more kmers that exist only in Peru and Egypt than in Ghana and Egypt.
+This implies that geographic region and proximity don't have the only, or at least not the greatest, impact on the mutation and similarity of the virus.
+Of course, this data is hard to make conclusions about without also knowing how features like population size and density, disease response, and other factors played out in each country.
+It should also be noted that the size of the data sets vary quite a bit;
+both Ghana and Peru have around 100-200 total sequences in the fasta, but Egypt has around 700.
+The Egypt data's much larger scale could skew the results because the other countries' smaller sample sizes might mean their data does not well represent the country as a whole.
+One interesting observation is that Peru, which is the most geographically separated from the other two countries, also has the most unique 12mers of its own. This does suggest to some degree that geographic separation allows for the virus to develop in unique ways, but the process of development and similarity to other virus locations is more complex than simple location on the globe.
+
+"""
 
 # ╔═╡ Cell order:
 # ╠═cbb613d0-e913-11eb-0d6c-cb8b7e8c571a
@@ -117,8 +113,10 @@ bar(xaxis,yaxis, label = "", xaxis = "Locations", yaxis = "Kmer Count", bar_widt
 # ╠═c76fd317-869c-43d1-ad8f-735f13f2da46
 # ╠═cdf022a2-ccd4-4746-8008-8c77e29c3950
 # ╠═91ab0a31-cf34-4454-a8a6-3c7a644633dd
-# ╟─dfb58341-dc84-4c37-a729-9e7afef2e5ee
+# ╠═dfb58341-dc84-4c37-a729-9e7afef2e5ee
 # ╠═03ec5984-a462-410b-9403-24b4c0e94a21
+# ╠═da281e5e-afe7-4fec-a709-424493561783
+# ╠═24c15bf4-9079-4aa4-97dd-e383a34fc718
 # ╟─aacf8425-eeae-4f0d-b4b2-5dbd0daf5603
 # ╠═abeb5ee2-fd2d-4643-90ef-e1b86959ceed
 # ╠═f5c9f7d4-311c-4545-a0de-d3060d356055
@@ -127,10 +125,11 @@ bar(xaxis,yaxis, label = "", xaxis = "Locations", yaxis = "Kmer Count", bar_widt
 # ╠═3652390f-28cb-4497-af79-7b245f78a893
 # ╠═432fce45-9755-4221-a08b-73529b67d1b9
 # ╠═b1a52631-6d3d-4f99-8b2c-7fc4ae15012e
-# ╠═ad1f1d24-5f22-4d69-9338-cf8c1e1ec188
-# ╠═e74a273a-9451-4d09-b68a-796b57bc9245
-# ╠═e32867e2-81fa-4e70-ae11-bdd41c0358ad
 # ╟─be6670fa-a026-4838-b2f3-a94eee5c98b1
 # ╠═f93f92fa-0e75-47c3-8fce-93c720a38f2e
+# ╠═f5beccb9-b9fa-4b42-afd4-c6ffe0857409
+# ╠═3e10a923-60da-4db0-b856-de045d1faa5e
 # ╠═cf88bfcf-2350-43c2-9136-16fea916ff9a
+# ╠═626de1de-e12a-4e67-9d0c-eed4b9c74d1b
 # ╠═1eae0dbb-a20d-4daf-ab1b-c1ecc6b4858d
+# ╠═46c7eda6-98b7-4706-85c8-6922985e8f8a
--- notebooks/AsiaOverTime.jl
+++ notebooks/AsiaOverTime.jl
@@ -10,52 +10,15 @@ using BioinformaticsBISC195
 # ╔═╡ 8699ff31-b4ed-4f74-ad90-f11a0c1bb024
 using Plots
 
-# ╔═╡ 00e96e55-c2e2-460d-b9d1-035af4f93e2c
-#=function monthlycomparison(original, allSeq, totalMonths)
-	dates = Vector{String}()
-	for headers in allSeq[1]
-		#@info headers
-		header = fasta_header(headers)
-		#@info "header" header[2]
-		if length(header[2]) >= 7 #if its shorter than this then it does not have a month listed, so I can't use it and don't need it in the vector
-			push!(dates, header[2][1:7])
-			@info "header short" header[2][1:7]
-		end
-		#@info dates
-	end
-
-	
-	#filling out dateIndices to hold ints that represent the indices of the next correct sequences to grab
-	dateIndices = Vector()
-	targetMonth = 1
-	targetYear = 2020
-	for i in 1:totalMonths
-		targetString = string(targetYear, "-", lpad(targetMonth,2,"0"))
-		@info targetString
-		@info findfirst(targetString .== dates)
-		push!(dateIndices, findfirst(targetString .== dates))
-		if targetMonth == 12
-			targetMonth = 1
-			targetYear = targetYear+1
-		else
-			targetMonth = targetMonth+1
-		end
-	end
-	
-	score = Vector()
-	for index in dateIndices
-		@info "index" index
-		#@info original
-		#@info allSeq[2][index]
-		#@info "max"  maximum(swscorematrix(original, allSeq[2][index]))
-		push!(score, maximum(swscorematrix(original, allSeq[2][index])))
-	end
-	return score
-end	=#
-
 # ╔═╡ 31bb927a-c189-485b-8888-622baec1b594
 md"""
-Collecting the data for all sequences in asia from the covgen_asia.fasta file.
+First I have to collect the data for all sequences in asia from the covgen_asia.fasta file.
+
+I downloaded the files from the following website, making sure to add the collection date as part of the header:
+[NCBI](https://www.ncbi.nlm.nih.gov/labs/virus/vssi/#/virus?SeqType_s=Nucleotide&VirusLineage_ss=Severe%20acute%20respiratory%20syndrome%20coronavirus%202%20(SARS-CoV-2),%20taxid:2697049&VirusLineage_ss=Middle%20East%20respiratory%20syndrome-related%20coronavirus%20(MERS-CoV),%20taxid:1335626&VirusLineage_ss=Severe%20acute%20respiratory%20syndrome-related%20coronavirus,%20taxid:694009&QualNum_i=50&Completeness_s=complete&CreateDate_dt=2019-12-01T00:00:00.00Z%20TO%202021-07-20T23:59:59.00Z&CollectionDate_dr=2019-12-19T00:00:00.00Z%20TO%202021-07-20T23:59:59.00Z&Region_s=Asia)
+
+The data consists of all SARS, COV2, and MERS sequences from all of Asia from Dec 2019 to July 2021.
+The data is parsed using the parse_fasta function that stores both the headers and the sequences themselves.
 """
 
 # ╔═╡ bcfd3636-b637-4422-8e76-d57e8a3f51d9
@@ -63,7 +26,7 @@ genomes_asia = parse_fasta(joinpath(@__DIR__, "..", "data", "covgen_asia.fasta")
 
 # ╔═╡ bdc8235b-e5e3-4ac8-a32d-ec8352bf7a3b
 md"""
-startingSeq is the very first sequence I am using, from Dec 2019, that will be compared against the other sequences each month to see how the sequence changes over time. The very first entry in the fasta file happens to be the first date needed, so no need to do any special search for it.
+startingSeq is the very first sequence I am using, from Dec 2019, that will be compared against the other sequences each month to see how the sequence changes over time. The very first entry in the fasta file happens to be the first date needed, so there was no need to do any special search for it.
 """
 
 # ╔═╡ 9e89e7d1-36ff-4337-b57d-769cf5aaed3e
@@ -71,79 +34,38 @@ startingSeq = genomes_asia[2][1]
 
 # ╔═╡ 88756434-059e-4335-a34c-7947a7571625
 md"""
-Now the final calculations can be performed sing the monthlycomparison function. I initally wanted to make the total months be closer to 16 in order to get more data, but that ended being very time consuming so I cut it down to 12 in order to get a full year in.
+Now the final calculations can be performed sing the monthlycomparison function. I initally wanted to make the total months be closer to 16 in order to get more data, but that ended up being very time and memory consuming so I cut it down to 12 in order to get a full year minimum in. To make things more efficient in a future version I would not use Smith-Waterman scoring, but it was interesting to see how the algorithm comparison system that we coded our selves works with real data.
 """
 
 # ╔═╡ a745b6cd-9db9-4caf-869f-bab219f61020
-monthly_scores = monthlycomparison(startingSeq, genomes_asia, 2)
-
-# ╔═╡ aff348a5-04f9-4d7c-a596-afb46309af04
-#=function test()
-	genomes_peru = parse_fasta(joinpath(@__DIR__, "..", "data", "covgen_peru.fasta"))
-	headers_peru = Vector()
-	for headers in genomes_peru[1]
-		@info headers
-		push!(headers_peru, fasta_header(string(">", headers)))
-	end
-	headers_peru
-	#println(headers_peru)
-	#monthlycomparison()
-	dates = Vector{String}()
-	for headers in genomes_peru[1]
-		header = fasta_header(string(">", headers))
-		push!(dates, header[2][1:7])
-	end
-	#println(dates)
-	dateIndices = Vector()
-	targetMonth = 1
-	targetYear = 2020
-	totalMonths = 16
-	for i in 1:totalMonths
-		targetString = string(targetYear, "-", lpad(targetMonth,2,"0"))
-		@info targetString
-		push!(dateIndices, findfirst(targetString .== dates))
-		@info targetYear
-		@info targetMonth
-		if targetMonth == 12
-			@info "its 12"
-			targetMonth = 1
-			targetYear = targetYear+1
-		else
-			@info "its not 12"
-			targetMonth = targetMonth+1
-		end
-	end
-	return dateIndices
-end=#
+monthly_scores = monthlycomparison(startingSeq, genomes_asia, 12)
 
 # ╔═╡ ef66799f-63c2-4bee-96b5-0bca9e614db8
 md"""
 Now I am going to make a scatter plot where the x axis is months since Dec 2019 and the y axis is the Smith-Waterman score for that month.
-I predict the trendline will gradually decrease as the months pass because a lower SW score indicates less similarity, and as the virus mutates and develops it will get less and less similar to the original strand from Dec 2019.
+I predict the scores will gradually decrease as the months pass because a lower SW score indicates less similarity, and as the virus mutates and develops it will get less and less similar to the original strand from Dec 2019.
 """
 
 # ╔═╡ fce9f89f-8573-483e-816a-e8930ba8d79b
-plot([1:2], monthly_scores, label = "", xaxis = "Months Since Dec 2019", yaxis = "Smith-Waterman Score", lw = 2, markershape = :circle)
+plot([1:12], monthly_scores, label = "", xaxis = "Months Since December 2019", yaxis = "Smith-Waterman Score", lw = 2, markershape = :circle)
 
 # ╔═╡ 6a505319-edb4-4c6d-b716-a84539d1a99f
 md"""
-It is difficult to determine a conclusive trend for the graph, though there are a few areas where there is a brief downward trend (from months 2-8 with an outlier at 6).
+It is difficult to determine a conclusive trend for the graph, though there are a few areas where there is a brief downward trend (from months 2-10 with score spikes/outliers at months 6 and 9).
 It is important to note the scale of the graph- the scores have a range of around 300, which seems like a big range for a data set of this size. 
-However, this is a rather limited sample of only one sequence per month being tested for an entire continent, and also only encompasses one year, so it is not a very accurate representation of the data as a whole.
+However, this is a rather limited sample of only one sequence per month being tested for an entire continent, ignoring the distinctions that may exist between individual countries, and also only encompasses one year, so it is not a very accurate representation of the data as a whole.
 It would be interesting to see if this trend continues for future months, or how it compares to different scoring algorithms or a more comprehensive method of scoring across many sequences.
 """
 
 # ╔═╡ Cell order:
 # ╠═ac9a9a63-808b-4353-b55c-e9e27414f3ed
 # ╠═8699ff31-b4ed-4f74-ad90-f11a0c1bb024
-# ╠═00e96e55-c2e2-460d-b9d1-035af4f93e2c
 # ╟─31bb927a-c189-485b-8888-622baec1b594
 # ╠═bcfd3636-b637-4422-8e76-d57e8a3f51d9
 # ╟─bdc8235b-e5e3-4ac8-a32d-ec8352bf7a3b
 # ╠═9e89e7d1-36ff-4337-b57d-769cf5aaed3e
 # ╟─88756434-059e-4335-a34c-7947a7571625
 # ╠═a745b6cd-9db9-4caf-869f-bab219f61020
-# ╠═aff348a5-04f9-4d7c-a596-afb46309af04
 # ╟─ef66799f-63c2-4bee-96b5-0bca9e614db8
 # ╠═fce9f89f-8573-483e-816a-e8930ba8d79b
 # ╟─6a505319-edb4-4c6d-b716-a84539d1a99f
